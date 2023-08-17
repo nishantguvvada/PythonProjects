64a887c22797849bbe3ffa59e6fb2592c5d7a0eb
nishantguvvada <nishant.guvvada@gmail.com>
2023-08-17 12:08:50 +0530

    Add files via upload

---
+++ DownloadOutlookAttachment.py
@@ -0,0 +1,25 @@
+import datetime
+import os
+import win32com.client
+
+path = os.path.expanduser(r'C:\Users\nish3395\OneDrive - Rackspace Inc\Documents\Automation')
+
+today = datetime.date.today()
+
+outlook = win32com.client.Dispatch("Outlook.Application").GetNamespace("MAPI")
+inbox = outlook.GetDefaultFolder(6)
+messages = inbox.Items
+
+def saveattachemnts(subject):
+    for message in messages:
+        if message.Subject == subject and message.Senton.date() == today:
+            # body_content = message.body
+            attachments = message.Attachments
+            attachment = attachments.Item(1)
+            for attachment in message.Attachments:
+                attachment.SaveAsFile(os.path.join(path, str(attachment)))
+                if message.Subject == subject and message.Unread:
+                    message.Unread = False
+                break
+
+saveattachemnts('Test - Subject')
